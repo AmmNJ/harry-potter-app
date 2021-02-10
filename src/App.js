bfa9b9deb8f087e1b697f46f4af8102f88f22926
Ammersken Nils <nilsammersken@gmail.com>
2021-02-10 16:35:10 +0100

    Add fonts

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ export default function App() {
     .catch(error => handleGetCharacterError(error))
 
   function createCards(characters) {
-    const cards = characters.map(character => Card(character.name))
+    const cards = characters.map(character =>
+      Card(character.name, character.yearOfBirth)
+    )
     document.body.append(...cards)
   }
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,23 @@
 import './Card.css'
 import createElement from '../../lib/createElement'
 
-export default function Card(name) {
-  return createElement('section', { className: 'Card' }, name)
+export default function Card(name, yearOfBirth) {
+  const nameEl = createElement('h2', {
+    className: 'Card__header',
+    innerText: name,
+  })
+
+  const yearOfBirthEl = createElement('span', {
+    className: 'Card__info',
+    innerText: yearOfBirth,
+  })
+
+  const el = createElement(
+    'section',
+    { className: 'Card' },
+    nameEl,
+    yearOfBirthEl
+  )
+
+  return el
 }
