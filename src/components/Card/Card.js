66bb5f878f6f5830aa0aab1bfc83f8330c9a0ccb
Ammersken Nils <nilsammersken@gmail.com>
2021-02-12 14:29:10 +0100

    Add image to card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,44 @@
 import './Card.css'
 import createElement from '../../lib/createElement'
 
-export default function Card(name, yearOfBirth) {
-  const nameEl = createElement('h2', {
-    className: 'Card__header',
-    innerText: name,
-  })
+export default function Card({ name, yearOfBirth, house, image }) {
+  const nameEl = createElement(
+    'h2',
+    {
+      className: 'Card__name',
+    },
+    name
+  )
 
-  const yearOfBirthEl = createElement('span', {
-    className: 'Card__info',
-    innerText: yearOfBirth,
+  const yearOfBirthEl = createElement(
+    'span',
+    {
+      className: 'Card__year-of-birth',
+    },
+    yearOfBirth
+  )
+
+  const houseEl = createElement(
+    'span',
+    {
+      className: 'Card__house',
+    },
+    house
+  )
+
+  const imageEl = createElement('img', {
+    className: 'Card__image',
+    src: image,
+    alt: '',
   })
 
   const el = createElement(
     'section',
     { className: 'Card' },
     nameEl,
-    yearOfBirthEl
+    yearOfBirthEl,
+    houseEl,
+    imageEl
   )
 
   return el
